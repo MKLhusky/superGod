508abe13e44f663d2a1252e9dc3cb60b705a3d30
daiqimeng <woshidaiqimeng@qq.com>
2023-03-23 22:18:47 +0800

    修改项目结构

--- system/super-common/src/main/java/com/system/supercommon/SuperCommonApplication.java
+++ system/super-common/src/main/java/com/system/supercommon/SuperCommonApplication.java
@@ -1,13 +1,10 @@
 package com.system.supercommon;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SuperCommonApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SuperCommonApplication.class, args);
     }
 
 }
--- system/super-system/src/main/java/com/system/supersystem/SuperSystemApplication.java
+++ system/super-system/src/main/java/com/system/supersystem/SuperSystemApplication.java
@@ -2,8 +2,9 @@ package com.system.supersystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SuperSystemApplication {
 
     public static void main(String[] args) {
