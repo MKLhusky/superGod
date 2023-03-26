91559da34184e48e5e66bcab05d74b169858e2fc
范文鹏 <fanwenpeng@51rcar.com>
2023-03-26 09:39:06 +0800

    打卡系统创建

---
+++ super-daily-attendance/daily-attendance/src/main/java/org/fd/dailyattendance/DailyAttendanceApplication.java
@@ -0,0 +1,13 @@
+package org.fd.dailyattendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyAttendanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyAttendanceApplication.class, args);
+    }
+
+}
