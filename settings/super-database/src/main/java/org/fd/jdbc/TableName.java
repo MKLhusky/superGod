495454b6361d58349607acca512dd9fe375b9c2b
daiqimeng <woshidaiqimeng@qq.com>
2023-07-25 20:02:45 +0800

    修改tableName注解没有加运行时注解和作用范围注解

--- settings/super-database/src/main/java/org/fd/jdbc/TableName.java
+++ settings/super-database/src/main/java/org/fd/jdbc/TableName.java
@@ -1,10 +1,15 @@
 package org.fd.jdbc;
 
+import java.lang.annotation.*;
+
 /**
  * @Description: 配合数据库jdbc工具类使用的注解  标注表名称
  * @Author: Mr. Dai
  * @Date: 2023/7/18 20:17
  **/
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface TableName {
     public String value() default "";
 }
