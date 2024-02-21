88887732de673071eb7ded5dc7c7596e37f94026
daiqimeng <woshidaiqimeng@qq.com>
2024-02-21 14:04:02 +0800

    加入wasm界面

---
+++ dioxus-web/README.md
@@ -0,0 +1,21 @@
+# Overview
+
+**This repo is not intended to be `git cloned`**.
+
+This repo is used by `dx create` when starting new projects. So by running `dx create` you are effectively running this code.
+
+# Development
+
+
+
+
+Run the following command in the root of the project to start the Dioxus dev server:
+
+```bash
+dx serve --hot-reload
+```
+
+- Open the browser to http://localhost:8080
+
+
+
---
+++ dioxus-web/src/main.rs
@@ -0,0 +1,57 @@
+#![allow(non_snake_case)]
+
+use dioxus_router::prelude::*;
+
+use dioxus::prelude::*;
+use log::LevelFilter;
+
+fn main() {
+    // Init debug
+    dioxus_logger::init(LevelFilter::Info).expect("failed to init logger");
+    console_error_panic_hook::set_once();
+
+    log::info!("starting app");
+    dioxus_web::launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    render! {
+        Router::<Route> {}
+    }
+}
+
+#[derive(Clone, Routable, Debug, PartialEq)]
+enum Route {
+    #[route("/")]
+    Home {},
+    #[route("/blog/:id")]
+    Blog { id: i32 },
+}
+
+#[component]
+fn Blog(cx: Scope, id: i32) -> Element {
+    render! {
+        Link { to: Route::Home {}, "Go to counter" }
+        "Blog post {id}"
+    }
+}
+
+#[component]
+fn Home(cx: Scope) -> Element {
+    let mut count = use_state(cx, || 0);
+
+    cx.render(rsx! {
+        Link {
+            to: Route::Blog {
+                id: *count.get()
+            },
+            "Go to blog"
+        }
+        div {
+            h1 { "High-Five counter: {count}" }
+            button { onclick: move |_| count += 1, "Up high!" }
+            button { onclick: move |_| count -= 1, "Down low!" }
+
+        }
+    })
+}
